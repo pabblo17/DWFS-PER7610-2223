6778708b0353e07a58873f8334e9d1c046e98b4f
jplacesc <jplacesc@unemi.edu.ec>
2022-12-11 16:40:12 -0500

    tema5 ejercicio1

---
+++ Tema_5/Jussibeth-Places/app/src/components/App.js
@@ -0,0 +1,14 @@
+import logo from "./../logo.svg";
+import "./../styles/App.css";
+import Navbar from "./Navbar";
+import Card from "./Card";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
