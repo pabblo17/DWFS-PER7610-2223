131df8563ad4cd05f92754905804f02a9efe685e
HENRY JIMENEZ ROSERO <henryjimenez.rosero@gmail.com>
2022-12-27 22:28:11 -0500

    Add files via upload

---
+++ Tema_5/Henry Jimenez Rosero/src/components/Sheeps.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Sheeps.css';
+const Sheeps = () =>{
+  return (
+    <div>
+        <h3 className='iniciosheeps'>Acerca de</h3>
+        <div className='ini'>
+             </div>
+        <div>
+     </div>
+    </div>
+  );
+}
+export default Sheeps;
